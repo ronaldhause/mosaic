1be6ffb1c31e1eaad1e967f4701989beceb8dcee
ronaldhause <ronaldhause@gmail.com>
2016-10-02 22:44:11 -0700

    cleaning up repo organization

--- cancer_gene_figs.r
+++
@@ -1,4 +0,0 @@
-load('/net/shendure/vol3/data/nobackup/hauser/Shendure/msi/tcga_loci_results_050315.robj')
-load('/net/shendure/vol3/data/nobackup/hauser/Shendure/msi/sampledata_042315.robj')
-load('/net/shendure/vol3/data/nobackup/hauser/Shendure/msi/sampleinfo_042315.robj')
-
